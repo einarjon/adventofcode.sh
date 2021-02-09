91926ff71bd57f73b370651018a9f6de827be633
Einar Jon Gunnarsson <tolvupostur@gmail.com>
2021-02-09 11:56:57 +0100

    17: Code reuse
    Single function for 3 and 4 dimensions.
    Index manipulation like this is not recommended.

--- 17.sh
+++ 17.sh
@@ -1,50 +1,26 @@
 #! /usr/bin/env bash
 A=($(< "${1:-17.txt}" ))
-declare -A B B2
+declare -A B CUBES
 N=6
 idx=({-1..18}) # 3xN
 for y in "${!A[@]}"; do
-    line=($(grep -o . <<< ${A[y]}))
+    line=($(grep -o . <<< "${A[y]}"))
     for x in "${!line[@]}"; do
-        [ "${line[x]}" == "#"  ] && B[$((x+N)).$((y+N)).$N]=1
+        [ "${line[x]}" == "#"  ] && CUBES[$((x+N)).$((y+N)).$N.$N]=1
     done
 done
-for i in "${!B[@]}"; do B2[$i]=1; done
 
 solve17() {
     local -A C=()
+    # index fuckery. For 3D, "${IDX[@]:xyz[3]:3}" will only loop through a single $N
+    local -a IDX[$N]=$N
+    if [ "$1" == 4 ]; then IDX=("${idx[@]}"); fi
     for i in "${!B[@]}"; do
         xyz=(${i//./ })
         for x in "${idx[@]:xyz[0]:3}"; do
             for y in "${idx[@]:xyz[1]:3}"; do
                 for z in "${idx[@]:xyz[2]:3}"; do
-                    C[$x.$y.$z]+=1
-                done
-            done
-        done
-        C[$i]=${C[$i]/1} # don't neighbour yourself
-    done
-    for i in "${!C[@]}"; do
-        if [[ -n "${B[$i]}" ]]; then
-            [[ ${#C[$i]} == 2 || ${#C[$i]} == 3 ]] || unset "B[$i]"
-        else
-            [[ ${#C[$i]} == 3 ]] && B[$i]=1
-        fi
-    done
-}
-for _ in "${idx[@]:1:N}"; do
-    solve17
-done
-echo "17A: ${#B[@]}"
-
-solve17B() {
-    local -A C=()
-    for i in "${!B[@]}"; do
-        xyz=(${i//./ })
-        for x in "${idx[@]:xyz[0]:3}"; do
-            for y in "${idx[@]:xyz[1]:3}"; do
-                for z in "${idx[@]:xyz[2]:3}"; do
-                    for w in "${idx[@]:xyz[3]:3}"; do
+                    for w in "${IDX[@]:xyz[3]:3}"; do
                         C[$x.$y.$z.$w]+=1
                     done
                 done
@@ -60,9 +36,15 @@ solve17B() {
         fi
     done
 }
-B=() # Clear and set to x.y.z.w
-for i in "${!B2[@]}"; do B[$i.$N]=1; done
+
+B=(); for i in "${!CUBES[@]}"; do B[$i]=1; done
 for _ in "${idx[@]:1:N}"; do
-    solve17B
+    solve17 3
+done
+echo "17A: ${#B[@]}"
+
+B=(); for i in "${!CUBES[@]}"; do B[$i]=1; done
+for _ in "${idx[@]:1:N}"; do
+    solve17 4
 done
 echo "17B: ${#B[@]}"
