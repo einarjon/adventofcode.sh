70f8d698718065f6bd8b6e384c261cea4e72e3b0
Einar Jon Gunnarsson <tolvupostur@gmail.com>
2021-03-26 10:19:32 +0100

    20: Some steps towards solving part 2
    Some assembly required

--- 20.sh
+++ 20.sh
@@ -1,20 +1,40 @@
 #! /usr/bin/env bash
 IFS=$'\n'
-A=($(tr '.' '_' < "${1:-20.txt}"))
-T=($(printf "%s\n" "${A[@]}" | grep -o "[0-9]*" ))
-U=($(printf "%s\n" "${A[@]}" | grep -A1 T | grep -E "[_#]" ))
-D=($(printf "%s\n" "${A[@]}" T | grep -B1 T | grep -E "[_#]" ))
-IFS=$' T' L=($(printf "%s\n" "${A[@]}" | cut -c1 | tail +2 | tr -d '\n '))
-IFS=$' :' R=($(printf "%s\n" "${A[@]}" | cut -c${#U[1]} | tail +2 | tr -d '\n '))
-IFS=$'\n'
-Ur=($(printf "%s\n" "${U[*]}" | rev))
-Dr=($(printf "%s\n" "${D[*]}" | rev))
-Lr=($(printf "%s\n" "${L[*]}" | rev))
-Rr=($(printf "%s\n" "${R[*]}" | rev))
-ENDS=() x=0
+A=($(tr '.' '_' < "${1:-20.txt}")) # . is a wildcard
+IFS=$' \t\n'
+NUM=($(printf "%s\n" "${A[@]}" | grep -o "[0-9]*" ))
+ENDS=() N=${#NUM[@]} n=${#A[1]}; A90=() # rotated 90deg counterclockwise
+for ((i=n;i>0;i--));do A90+=($(printf "%s\n" "${A[@]}" | cut -c$i | tr -d '\n')); done
+U=($(printf "%s\n" "${A[@]}" | grep -A1 Tile | grep -E "[_#]" ))
+D=($(printf "%s\n" "${A[@]}" Tile | grep -B1 Tile | grep -E "[_#]" ))
+L=(${A90[-1]//[^_#]/ })
+R=(${A90[0]//[^_#]/ })
+Ur=($(printf "%s\n" "${U[@]}" | rev))
+Dr=($(printf "%s\n" "${D[@]}" | rev))
+Lr=($(printf "%s\n" "${L[@]}" | rev))
+Rr=($(printf "%s\n" "${R[@]}" | rev))
+printf -v ALL "%s\n" "${U[@]}" "${R[@]}" "${Dr[@]}" "${Lr[@]}" "${Ur[@]}" "${Rr[@]}" "${D[@]}" "${L[@]}"
 for i in "${!U[@]}"; do
-     x=$(printf "%s\n" "${U[*]}" "${Ur[*]}" "${D[*]}" "${Dr[*]}" "${L[*]}" "${Lr[*]}" "${R[*]}" "${Rr[*]}" | grep -c -e "${U[i]}" -e "${Dr[i]}" -e "${Lr[i]}" -e "${R[i]}" )
-    [ "$x" = 6 ] && ENDS+=("${T[i]}")
+    x=($(echo "$ALL" | grep -n -E -x "(${U[i]}|${R[i]}|${Dr[i]}|${Lr[i]})" | cut -d: -f1 | \
+        while read k; do (( --k >= 4*N || (k%N)!=i )) && echo "$((k/N)):$((k%N))"; done))
+    case ${#x[*]} in
+        2) ENDS[i]="${NUM[i]}"; CORNERS[i]="${x[*]}";;
+        3) SIDES[i]="${x[*]}";;
+        4) MIDDLE[i]="${x[*]}";;
+        *) echo "ERROR($i): ${x[*]}"; break;;
+    esac
 done
 printf -v sum "*%s" "${ENDS[@]}"; sum=${sum:1}
 echo "20A: $sum=$((sum))"
+
+all="${A[*]}"; all=${all//[^#]}; echo total=${#all}
+edges=$(for i in "${!U[@]}"; do
+    printf "%s" "${U[i]:1}" "${Dr[i]:1}" "${L[i]:1}" "${Rr[i]:1}"
+done |  tr -cd '#' | wc -c)
+monster=\
+"..................#.
+#....##....##....###
+.#..#..#..#..#..#..."
+monsterlen=${monster//[^\#]}; monsterlen=${#monsterlen}
+regex=${monster/................../[.\#]\{18\}} # no newline at start
+regex=${regex//$'\n'/.\{76\}}                   # newline can be anywhere
