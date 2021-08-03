82ad1a70402e6452f52a9ccf1b20a5d8fdd78c8e
Einar Jon Gunnarsson <tolvupostur@gmail.com>
2021-08-03 14:01:47 +0200

    22: Fix error in final calculation
    Broken in commit 3d72e77bc18b1a4f43800541749885418cde04b.
    "${A[*]}" is a single string, while ${A[*]} and "${A[@]}" is a list.
    Minor cleanup
    
    Thanks to Bruno Raoult for pointing this out

--- 22.sh
+++ 22.sh
@@ -1,48 +1,44 @@
 #! /usr/bin/env bash
 C=($(grep "^[0-9]" "${1:-22.txt}"))
-A=(${C[@]:0:${#C[@]}/2})
-B=(${C[@]:${#C[@]}/2})
+N=${#C[@]}
+A=(${C[@]:0:N/2})
+B=(${C[@]:$N/2})
 
 score() {
     local sum=0 S=("$@")
-    for i in "${!S[@]}"; do ((sum+=S[i]*(50-i))); done
+    for i in "${!S[@]}"; do ((sum+=S[i]*(N-i))); done
     echo $sum
 }
 
-#round=0
-while [[ ${#A[@]} != 0 && ${#B[@]} != 0 ]]; do
+# shellcheck disable=SC2128
+while [[ -n "$A" && -n "$B" ]]; do
     if [[ $A -gt $B ]]; then A+=($A $B); else B+=($B $A); fi
     A=(${A[@]:1})
     B=(${B[@]:1})
     #((++round%500==0)) && echo "$round: ${#A[@]}/${#B[@]}"
 done
-echo "22A: $(score "${A[*]}" "${B[*]}")"
+echo "22A: $(score "${A[@]}" "${B[@]}")"
 
 r() {
-  local X="$*"
   declare -A H
-  local a=(${X/ X*}) b=(${X/*X }) hash
-  while [[ ${#a[@]} != 0 && ${#b[@]} != 0 ]]; do
+  local a=($1) b=($2) hash
+  # shellcheck disable=SC2128,SC2181
+  while [[ -n "$a" && -n "$b" ]]; do
     hash=${a[*]}X${b[*]}
-    hash=${hash// /_}
-    if [[ -n "${H[$hash]}" ]]; then
+    if (( H[${hash// /_}]++ > 0)); then
         return 0
     elif [[ $a -lt ${#a[@]} && $b -lt ${#b[@]} ]];  then
-        r "${a[*]:1:a}" X "${b[*]:1:b}"
+        r "${a[*]:1:a}" "${b[*]:1:b}"
     else
         [[ $a -gt $b ]]
     fi
-    # shellcheck disable=SC2181,SC2128
     if [[ $? == 0 ]]; then a+=($a $b); else b+=($b $a); fi
-    H[$hash]=1
     a=(${a[@]:1})
     b=(${b[@]:1})
     #((${#H[@]}%1000==0)) && echo "$round:${#H[@]}: ${#a[@]}/${#b[@]}"
   done
-  ((${#a[*]}+${#b[*]} == 50)) && echo "22B: $(score "${a[*]}" "${b[*]}")"
+  ((${#a[*]}+${#b[*]} == N)) && echo "22B: $(score "${a[@]}" "${b[@]}")"
   return ${#b[@]}
 }
 
-A=(${C[@]:0:${#C[@]}/2})
-B=(${C[@]:${#C[@]}/2})
-r "${A[*]}" X "${B[*]}"
+r "${C[*]:0:N/2}" "${C[*]:N/2}"
