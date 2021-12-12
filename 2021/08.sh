a3a8c5d12670d514f5b1c1be783beb3f03f5144a
Einar Jon Gunnarsson <tolvupostur@gmail.com>
2021-12-12 21:40:15 +0100

    2021/08: Simplify logic
    No need to collect every number to the T array.
    Just identify 4 and 7, and use those to find the
    differences between 2,3,5 and 6,9,0.

--- 2021/08.sh
+++ 2021/08.sh
@@ -4,39 +4,28 @@ printf "8A: "
 cut -d\| -f 2 "$input" | grep -Eo "[a-g]+" | grep -cE "^([a-g]{2,4}|[a-g]{7})$"
 sum=""
 while read -r -a A; do
-    T=() P235=() P690=() n=""
+    T4="" T7="" n=""
     for i in "${A[@]:0:10}"; do
-        case ${#i} in
-            2) T[1]=$i;;
-            3) T[7]=$i;;
-            4) T[4]=$i;;
-            5) P235+=($i);;
-            6) P690+=($i);;
-            7) T[8]=$i;;
+        case ${#i} in # Just find 4 and 7, ignore the rest
+            3) T7=$i; [[ -n $T4 ]] && break;;
+            4) T4=$i; [[ -n $T7 ]] && break;;
         esac
-    done
-    for i in "${P235[@]}"; do
-        if [[ ${i//[${T[4]}]} == ??? ]]; then  T[2]=$i
-        elif [[ ${i//[${T[7]}]} == ?? ]]; then T[3]=$i
-        else                                   T[5]=$i
-        fi
-    done
-    for i in "${P690[@]}"; do
-        if [[ ${i//[${T[7]}]} == ???? ]]; then T[6]=$i
-        elif [[ ${i//[${T[4]}]} == ?? ]]; then T[9]=$i
-        else                                   T[0]=$i
-        fi
     done
     for i in "${A[@]:11}"; do
-        case ${#i} in
-            2) n+=1;;
-            3) n+=7;;
-            4) n+=4;;
-            5) for k in 2 3 5 ?; do [[ -z ${i//[${T[k]}]} ]] && break; done; n+=$k;;
-            6) for k in 6 9 0 ?; do [[ -z ${i//[${T[k]}]} ]] && break; done; n+=$k;;
-            7) n+=8;;
+        case ${#i},${i//[$T4]},${i//[$T7]} in
+            2,*)      n+=1;;
+            3,*)      n+=7;;
+            4,*)      n+=4;;
+            5,???,*)  n+=2;;
+            5,*,??)   n+=3;;
+            5,*)      n+=5;;
+            6,*,????) n+=6;;
+            6,??,*)   n+=9;;
+            6,*)      n+=0;;
+            7,*)      n+=8;;
+            *) echo ERROR: ${#i},${i//[$T4]},${i//[$T7]}; break 2;;
         esac
     done
-    sum+=+$((10#$n)) # leading 0 means octal. A '?' will throw an error here
+    sum+=+$((10#$n))
 done < "${input}"
 echo "8B: $((sum))"
